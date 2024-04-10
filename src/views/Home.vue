ebcd8502bd179f8ad8e54878a63a3d8844dd50aa
Leonardo Ramos <leonardo.a.ramos@accenture.com>
2024-04-10 11:52:46 +0100

    Burguer project using Vue3

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+      <BurguerForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue";
+import BurguerForm from "../components/BurguerForm.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    BurguerForm,
+  },
+};
+</script>
